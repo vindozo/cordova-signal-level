85d9177713b91e3a7ee824e0f8ef889e23b1614a
vindozo <vindozo@gmail.com>
2020-06-27 17:57:14 +0500

    Update SignalLevel.js

--- www/SignalLevel.js
+++ www/SignalLevel.js
@@ -1,5 +1,22 @@
 module.exports = {
-    get: function (successCallback, errorCallback) {
+    getjsonresult: function (successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "SignalLevel", "getjsonresult", []);
+    },
+    
+    get: function (successCallback, errorCallback) {
+        SignalLevel.successCallback = successCallback;
+        SignalLevel.errorCallback = errorCallback;
+        SignalLevel.getjsonresult(function(resp){
+           resp = JSON.parse(resp);
+           SignalLevel.successCallback( {
+               isConnectedWifi: !!JSON.parse(String(resp.isConnectedWifi).toLowerCase()), 
+               isConnectedMobile: !!JSON.parse(String(resp.isConnectedMobile).toLowerCase()), 
+               WifiSignalLevel: (resp.WifiSignalLevel-0), 
+               MobileSignalLevel: (resp.MobileSignalLevel-0), 
+               ConnectedFast: resp.ConnectedFast 
+           });
+        }, function(err){
+            SignalLevel.errorCallback(err);
+        });
     }
 };
